ba37df5ffcbe3fea8951264bbb1cdd1da365bbcb
yonelyvan <yonelyvan@gmail.com>
2018-09-12 17:56:54 -0500

    galeria

--- holamundo/app/src/main/java/com/example/ynl/holamundo/LastActivity.java
+++ holamundo/app/src/main/java/com/example/ynl/holamundo/LastActivity.java
@@ -1,23 +1,25 @@
 package com.example.ynl.holamundo;
 
+import android.content.Intent;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.FrameLayout;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class LastActivity extends AppCompatActivity {
-
+    public int id_camera = 0;
     static  Camera camera = null;
     FrameLayout frameLayout;
     ShowCamera showCamera;
 
+    private static final int PICK_IMAGE =100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +27,10 @@ public class LastActivity extends AppCompatActivity {
         //
         frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
         //abrir camara
-        camera = Camera.open();
+        camera = Camera.open(id_camera);
         showCamera = new ShowCamera(this,camera);
         frameLayout.addView(showCamera);
     }
-
 
     Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
         @Override
@@ -60,7 +61,7 @@ public class LastActivity extends AppCompatActivity {
             if(!folder_gui.exists()){
                 folder_gui.mkdir();
             }
-            File outputFile = new File(folder_gui,"temp.jpg");
+            File outputFile = new File(folder_gui,"temp1.jpg");
             return outputFile;
         }
     }
@@ -70,6 +71,30 @@ public class LastActivity extends AppCompatActivity {
             camera.takePicture(null,null, mPictureCallback);
         }
     }
+
+    public void switchCameraClick(View v){
+        //camera.stopPreview();
+        //camera.release();
+        //swap the id of the camera to be used
+        if(id_camera == 1){
+            id_camera= 0;
+        }else {
+            id_camera = 1;
+        }
+        //camera.startPreview();
+        //camera = Camera.open(id_camera);
+        camera = Camera.open(id_camera);
+
+        showCamera = new ShowCamera(this,camera);
+        frameLayout.addView(showCamera);
+        camera.startPreview();
+    }
+
+    public void openGallery(View v){
+        Intent gallery= new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery,PICK_IMAGE);
+
+        //this.sendBroadcast(gallery);
+    }
+
 }
-
-
