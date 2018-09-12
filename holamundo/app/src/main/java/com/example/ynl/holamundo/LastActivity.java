940412391280507e09d30e2d9f300034c543bf74
yonelyvan <yonelyvan@gmail.com>
2018-09-12 13:48:10 -0500

    guardado de imagen

--- holamundo/app/src/main/java/com/example/ynl/holamundo/LastActivity.java
+++ holamundo/app/src/main/java/com/example/ynl/holamundo/LastActivity.java
@@ -1,22 +1,16 @@
 package com.example.ynl.holamundo;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 public class LastActivity extends AppCompatActivity {
 
@@ -37,6 +31,45 @@ public class LastActivity extends AppCompatActivity {
     }
 
 
+    Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File picture_file = getOutputMediaFile();
+            if(picture_file == null){
+                return;
+            }else{
+                try {
+                    FileOutputStream fos = new FileOutputStream(picture_file);
+                    fos.write(data);
+                    fos.close();
+
+                    camera.startPreview();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    private File getOutputMediaFile(){
+        String state = Environment.getExternalStorageState();
+        if(!state.equals(Environment.MEDIA_MOUNTED)){
+            return null;
+        }else {
+            File folder_gui = new File(Environment.getExternalStorageDirectory() + "/DCIM/MYCAMERA");
+            if(!folder_gui.exists()){
+                folder_gui.mkdir();
+            }
+            File outputFile = new File(folder_gui,"temp.jpg");
+            return outputFile;
+        }
+    }
+
+    public void captureImage(View v){
+        if(camera!=null){
+            camera.takePicture(null,null, mPictureCallback);
+        }
+    }
 }
 
 
