6a07975dbbaf7f21813d53bea5d3b4bb5d9eeb3c
yonelyvan <yonelyvan@gmail.com>
2018-11-05 07:31:39 -0500

    ui upload

--- mnet/app/src/main/java/com/example/ynl/mnet/FragmentPlus.java
+++ mnet/app/src/main/java/com/example/ynl/mnet/FragmentPlus.java
@@ -1,12 +1,18 @@
 package com.example.ynl.mnet;
 
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,10 +21,12 @@ import com.google.firebase.auth.FirebaseUser;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentMuro extends Fragment {
+public class FragmentPlus extends Fragment {
     private FirebaseUser user;
+    public static final String TAG = "PLUS";
 
-    public FragmentMuro() {
+
+    public FragmentPlus() {
         // Required empty public constructor
     }
 
@@ -27,19 +35,23 @@ public class FragmentMuro extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         validar_sesion();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_muro, container, false);
+        return inflater.inflate(R.layout.fragment_plus, container, false);
     }
+
 
     public void validar_sesion(){
         user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             // User is signed in
-            Log.e("sesion iniciada:", user.getEmail());
+            Log.e("PLUS", "cuenta: "+user.getEmail());
         } else {
             // No user is signed in
-            Log.e("ERROR","no existe sesion:");
+            Log.e("PLUS ERROR","no existe sesion:");
         }
     }
+
+
+
 
 
 
--- mnet/app/src/main/java/com/example/ynl/mnet/MnetActivity.java
+++ mnet/app/src/main/java/com/example/ynl/mnet/MnetActivity.java
@@ -1,6 +1,12 @@
 package com.example.ynl.mnet;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -8,13 +14,28 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.File;
 
 public class MnetActivity extends AppCompatActivity {
     FragmentNotificaciones fragmentNotificaciones;
-    FragmentMuro fragmentMuro;
     FragmentDashboard fragmentDashboard;
+    FragmentPlus fragmentPlus;
+    FragmentMuro fragmentMuro;
     FragmentChat fragmentChat;
+
+    public static final String TAG = "MNET";
+    public String IMGPATH="null";
+    private static int RESULT_LOAD_IMAGE = 1;
+
+    //private StorageReference m_storage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -24,19 +45,23 @@ public class MnetActivity extends AppCompatActivity {
             switch (item.getItemId()) {
 
                 case R.id.navigation_notifications:
-                    Log.e("TAG:","Notificaciones");
+                    Log.e(TAG,"Notificaciones");
                     setFragment(fragmentNotificaciones);
                     return true;
                 case R.id.navigation_dashboard:
-                    Log.e("TAG:","dashboard");
+                    Log.e(TAG,"dashboard");
                     setFragment(fragmentDashboard);
+                    return true;
+                case R.id.navigation_plus:
+                    Log.e(TAG,"plus");
+                    setFragment(fragmentPlus);
                     return true;
                 case R.id.navigation_muro:
                     setFragment(fragmentMuro);
-                    Log.e("TAG:","MURO");
+                    Log.e(TAG,"MURO");
                     return true;
                 case R.id.navigation_chat:
-                    Log.e("TAG:","CHAT");
+                    Log.e(TAG,"CHAT");
                     setFragment(fragmentChat);
                     return true;
             }
@@ -53,8 +78,9 @@ public class MnetActivity extends AppCompatActivity {
 
         //
         fragmentNotificaciones = new FragmentNotificaciones();
-        fragmentMuro = new FragmentMuro();
         fragmentDashboard = new FragmentDashboard();
+        fragmentPlus = new FragmentPlus();
+        fragmentMuro = new FragmentMuro();
         fragmentChat = new FragmentChat();
 
         //
@@ -68,5 +94,100 @@ public class MnetActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.fragment_container, fragment);
         fragmentTransaction.commit();
     }
+
+    //plus: to upload photo
+    public void openGallery(View v){
+        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
+
+    //seleccion de imagen de galeria
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            Log.e(TAG,selectedImage.toString());//URI
+
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            //message img path and go to an activity
+            //Intent intent = new Intent(this,UploadPhoto.class);
+            //intent.putExtra(IMGPATH,picturePath);
+
+            //Intent intent = getIntent();
+            String imgpath = picturePath; //intent.getStringExtra(MainActivity.IMGPATH);
+            Log.e("SHOWIMG::",imgpath);
+            IMGPATH = imgpath;
+            ImageView imageView = (ImageView) findViewById(R.id.img_to_upĺoad);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+
+
+            //intent.putExtra(DATA_IMG, data);
+
+            Log.e(TAG, "IMAGEN GALLERY "+picturePath );//URI
+            //startActivity(intent);
+        }
+    }
+
+    public void cargar_foto(View v){
+        Intent intent = getIntent();
+        String imgpath = intent.getStringExtra(IMGPATH);
+        Log.e("ERRORRRRRRRRR::",imgpath);
+        File IMG_file = new File(imgpath);
+        Log.e("ERRORRRRRRRRR::",IMG_file.getPath());
+        Uri uri = Uri.fromFile(IMG_file);
+        //Uri uri = data.getData();
+
+        final ProgressDialog progressDialog =new ProgressDialog(this);
+        progressDialog.setTitle("Cargando...");
+        progressDialog.show();
+
+/*
+        StorageReference filePath = m_storage.child("fotos").child(uri.getLastPathSegment());
+
+
+        filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(com.example.ynl.camara.UploadPhoto.this,"Se subio exitosamente la foto",Toast.LENGTH_LONG).show();
+                progressDialog.hide();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(UploadPhoto.this,"Failed"+ e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                        double progress = 100.0*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount();
+                        progressDialog.setMessage("Cargando "+ (int)progress + "%");
+                    }
+                });
+*/
+
+        //uri to path
+        /*
+        String path = MainActivity.IMGPATH; //uri_to_path(uri);
+        Log.e(MainActivity.TAG,path);
+        ImageView imageView = (ImageView) findViewById(R.id.imgChoosed);
+        imageView.setImageBitmap(BitmapFactory.decodeFile(path));
+        */
+    }
+
+
+
+
 
 }
